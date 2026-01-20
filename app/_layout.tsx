b42495cfdd3bf82b89b64e5a89bf015151a519f1
Luis <luiswoldu@googlemail.com>
2026-01-20 14:14:13 -0500

    Colors (Tailwind.config.js)

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,7 +4,7 @@ import { Text, View } from "react-native";
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">
+      <Text className="text-xl font-bold text-primary-muted">
         Welcome to Nativewind!
       </Text>
     </View>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,25 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#6CD401',
+          highlight: '#98E14D',
+          muted: '#F0FBE5'
+        },
+        secondary: {
+          DEFAULT: '#F7F7F7',
+          muted: '#B2B2B2',
+          placeholder: '#9F9F9F',
+          active: '#58575C'
+        },
+        red: "#FE303F",
+      },
+      backgroundImage: {
+        "primary-gradient": "linear-gradient(90deg, #6ED308 0%, #A5E765 100%)",
+      },
+    },
   },
   plugins: [],
 }
