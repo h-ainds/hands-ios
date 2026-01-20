36a3ef266690308b27fdb5585f48ba900afde8d4
Luis <luiswoldu@googlemail.com>
2026-01-20 12:19:07 -0500

    NativeWind Installation

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -5,7 +5,7 @@ import { Text, View } from '@/components/Themed';
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Home</Text>
+      <Text style={styles.title}>Hoeme</Text>
     </View>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./app/**/*.{js,ts,tsx}",
-    "./components/**/*.{js,ts,tsx}",
-  ],
-  darkMode: "false",
+  // NOTE: Update this to include the paths to all files that contain Nativewind classes.
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
